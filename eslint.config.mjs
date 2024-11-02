9c20891cf0b6dbc47907880a8f860161431b9ab7
KuyaKoya <sethmcornelio@su.edu.ph>
2024-11-02 11:17:58 +0800

    chore: add prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-roots': 'off',
+      'vue/component-api-style': ['error', ['script-setup', 'composition']],
+      'vue/block-lang': [
+        'error',
+        {
+          script: {
+            lang: 'ts',
+          },
+        },
+      ],
+      'vue/component-tags-order': [
+        'error',
+        {
+          order: ['template', 'script', 'style'],
+        },
+      ],
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+  {
+    files: ['**/api/**/*.{get,post,put,patch,delete}.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: ['.nuxt/', '.output/', 'node_modules/', 'dist/', '*.md*', '*.json', 'generated', 'pnpm-lock.yaml'],
+  },
+);
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script lang="ts">
+const st5 = 'hello';
+if (st5 === 'hello') {
+  console.log('hello');
+}
+</script>
