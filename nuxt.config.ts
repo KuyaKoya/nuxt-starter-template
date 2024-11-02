98713a906b8358979e3c3ce1b0115fa82f3fefbc
KuyaKoya <sethmcornelio@su.edu.ph>
2024-11-02 12:11:59 +0800

    chore: add primevue and primeicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  srcDir: './src',
+  imports: {
+    dirs: ['./composables/**', './store/**', './utils'],
+  },
+  build: {
+    transpile: ['primevue'],
+  },
+  css: ['primeicons/primeicons.css'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@primevue/nuxt-module'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
   eslint: {
     config: {
       stylistic: {
         commaDangle: 'only-multiline',
+      },
+    },
+  },
+  primevue: {
+    usePrimeVue: true,
+    autoImport: true,
+    directives: {
+      include: ['Ripple', 'Tooltip'],
+    },
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
       },
     },
   },
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>hello</div>
+  <div class="flex flex-col gap-4">
+    <Avatar label="P" class="mr-2" size="xlarge" />
+    <Button label="Login" class="p-button-outlined w-fit" />
+    <Button icon="pi pi-home" aria-label="Save" />
+  </div>
 </template>
 
 <script lang="ts">
+const checked = ref(false);
 const st5 = 'hello';
 if (st5 === 'hello') {
   console.log('hello');
