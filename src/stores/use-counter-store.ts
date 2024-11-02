e78dfbced5bc86b599ec704521591ad1b22148aa
Seth Cornelio <seth.cornelio@mllrdev.com>
2024-11-02 23:07:45 +0800

    refactor: remove console.log

--- src/stores/use-counter-store.ts
+++ src/stores/use-counter-store.ts
@@ -2,7 +2,6 @@ export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
-    console.log('clicked')
     count.value++
   }
   return { count, doubleCount, increment }
